8d05d0d5258e16a83191346ee07f37fb7a41d68e
Your Name <your@email.com>
2019-04-09 19:07:18 +0000

    instaled and added ejs as template engine for express

--- express_server.js
+++ express_server.js
@@ -2,11 +2,15 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+//This tells the Express app to use EJS as its templating engine. 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
 
+//app.get are different response depending on wich root i am getting
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
@@ -19,6 +23,7 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//app.listen opens port from my terminal
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
